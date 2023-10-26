99950cc8e81b4b13fbe3626e75f5bfaf587026c7
oriionn <xemilien67@gmail.com>
2023-10-26 22:28:14 +0200

    chore(.env.example): add PORT variable with default value of 3000 for local development feat(app.js): add support for generating custom banners with project name and optional description feat(package.json): add dependencies for dotenv and express feat(templates/banner_error.svg): add template for error banner with customizable error message feat(templates/banner_with_description.svg): add template for banner with project name and description feat(templates/banner_without_description.svg): add template for banner with project name only

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+function generateError(err) {
+  let template = fs.readFileSync(path.join(__dirname, "templates", "banner_error.svg"), "utf8");
+  return template.replace("{{ERROR}}", err);
+}
+
+app.get("/", (req, res) => {
+  let project = req.query.name;
+  let description = req.query.description;
+
+  res.header("Content-Type", "image/svg+xml");
+  if (!project) return res.send(generateError("Aucun nom de projet est spécifié."));
+  if (project.length > 20) return res.send(generateError("Le nom du projet est trop long."));
+
+  if (description) {
+    if (description.length > 50) return res.send(generateError("La description est trop longue."));
+
+    let template = fs.readFileSync(path.join(__dirname, "templates", "banner_with_description.svg"), "utf8");
+    template = template.replace("{{NAME}}", project);
+    return res.send(template.replace("{{DESCRIPTION}}", description));
+  } else {
+    let template = fs.readFileSync(path.join(__dirname, "templates", "banner_without_description.svg"), "utf8");
+    return res.send(template.replace("{{NAME}}", project));
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+})
