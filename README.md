bf6a68177081db9d59b2476fe08ceec6d1812801
oriionn <xemilien67@gmail.com>
2023-10-26 22:49:21 +0200

    chore(README.md): update README.md with installation instructions, usage information, license, and contributors section fix(app.js): add support for splitting long descriptions into two lines in the banner template feat(templates/banner_with_description2.svg): add new template for banners with long descriptions, split description into two lines

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# bannerGenerator
-Little API for generate my custom banner for my projects
+# Banner Generator
+Little API for generate my custom banner for my projects.
+
+## Installation
+1. Clone this repository
+```bash
+git clone https://github.com/oriionn/bannerGenerator.git 
+```
+2. Go to the project folder
+```bash
+cd bannerGenerator
+```
+3. Install dependencies
+```bash
+npm install
+```
+4. Run the project
+```bash
+npm start
+```
+5. Open your browser and go to http://localhost:3000
+
+## Usage
+### GET /
+Return a custom banner
+#### Parameters
+| Name | Type | Description         |
+|------| --- |---------------------|
+| name | string | Name of the project |
+| description | string | Description of the project |
+
+#### Example
+```bash
+http://localhost:3000/?name=Banner%20Generator&description=Little%20API%20for%20generate%20my%20custom%20banner%20for%20my%20projects.
+```
+
+## License
+The project is under the [MIT License](LICENSE).
+
+## Contributors
+![Contributors](https://contrib.rocks/image?repo=oriionn/bannerGenerator)
--- app.js
+++ app.js
@@ -11,6 +11,24 @@ function generateError(err) {
   return template.replace("{{ERROR}}", err);
 }
 
+function divideDescription(description) {
+  let midpoint = Math.ceil(description.length / 2);
+
+  let firstSpace = description.lastIndexOf(' ', midpoint);
+  let secondSpace = description.indexOf(' ', midpoint);
+
+  let firstLine, secondLine;
+  if (firstSpace !== -1 && (secondSpace === -1 || midpoint - firstSpace < secondSpace - midpoint)) {
+    firstLine = description.substring(0, firstSpace);
+    secondLine = description.substring(firstSpace + 1);
+  } else {
+    firstLine = description.substring(0, secondSpace);
+    secondLine = description.substring(secondSpace + 1);
+  }
+
+  return [firstLine, secondLine];
+}
+
 app.get("/", (req, res) => {
   let project = req.query.name;
   let description = req.query.description;
@@ -20,11 +38,18 @@ app.get("/", (req, res) => {
   if (project.length > 20) return res.send(generateError("Le nom du projet est trop long."));
 
   if (description) {
-    if (description.length > 50) return res.send(generateError("La description est trop longue."));
-
-    let template = fs.readFileSync(path.join(__dirname, "templates", "banner_with_description.svg"), "utf8");
-    template = template.replace("{{NAME}}", project);
-    return res.send(template.replace("{{DESCRIPTION}}", description));
+    if (description.length > 50) {
+      if (description.length > 100) return res.send(generateError("La description est trop longue."));
+      let [firstLine, secondLine] = divideDescription(description);
+      let template = fs.readFileSync(path.join(__dirname, "templates", "banner_with_description2.svg"), "utf8");
+      template = template.replace("{{NAME}}", project);
+      template = template.replace("{{DESCRIPTION2}}", secondLine);
+      return res.send(template.replace("{{DESCRIPTION}}", firstLine));
+    } else {
+      let template = fs.readFileSync(path.join(__dirname, "templates", "banner_with_description.svg"), "utf8");
+      template = template.replace("{{NAME}}", project);
+      return res.send(template.replace("{{DESCRIPTION}}", description));
+    }
   } else {
     let template = fs.readFileSync(path.join(__dirname, "templates", "banner_without_description.svg"), "utf8");
     return res.send(template.replace("{{NAME}}", project));
